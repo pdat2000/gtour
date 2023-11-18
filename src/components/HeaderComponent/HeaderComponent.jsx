4ca10c2cbad186d94296026cc9e53cba8fd65ac0
Nig36 <106310322+pdat2000@users.noreply.github.com>
2023-11-18 23:06:33 +0700

    refactor header

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -10,6 +10,33 @@ import './style.scss'
 const HeaderComponent = () => {
   const menus =[
     {
+      id: 1,
+      name: 'Home',
+      link:'/',
+      items:[
+        {
+          id: 1,
+          link:'/',
+          name:'Home 1 – Background Image'
+        },
+        {
+          id: 2,
+          link:'/',
+          name:'Home 2 – Youtube Video'
+        },
+        {
+          id: 3,
+          link:'/',
+          name:'Home 3 – Google Inspired'
+        },
+        {
+          id: 4,
+          link:'/',
+          name:'Home 4 – Travel Site'
+        }
+      ]
+    },
+    {
       id: 2,
       name: 'Tours',
       link: '/',
@@ -39,17 +66,109 @@ const HeaderComponent = () => {
         {
           id: 2,
           link: '/',
-          name: 'Tour Classic Sidebar'
+          name: 'Tour Classic Sidebar',
+          items: [
+            {
+              id: 1,
+              name: 'Right Sidebar',
+              link: '/'
+            },
+            {
+              id: 2,
+              name: 'Left Sidebar',
+              link: '/'
+            },
+          ]
         },
         {
           id: 3,
           link: '/',
-          name: 'Tour Classic Sidebar'
+          name: 'Tour Grid Fullwidth',
+          items: [
+            {
+              id: 1,
+              name:'2 Columns',
+              link:'/'
+            },
+            {
+              id: 2,
+              name:'3 Columns',
+              link:'/'
+            },
+            {
+              id: 3,
+              name:'4 Columns',
+              link:'/'
+            },
+          ]
         },
         {
           id: 4,
           link: '/',
-          name: 'Home 1 – Background Image'
+          name: 'Tour Grid Sidebar',
+          items: [
+            {
+              id: 1,
+              name: 'Right Sidebar',
+              link: '/'
+            },
+            {
+              id: 2,
+              name: 'Left Sidebar',
+              link: '/'
+            },
+          ]
+        },
+        {
+          id: 5,
+          link: '/',
+          name: 'Tour List Sidebar',
+          items: [
+            {
+              id: 1,
+              name: 'Right Sidebar',
+              link: '/'
+            },
+            {
+              id: 2,
+              name: 'Left Sidebar',
+              link: '/'
+            },
+          ]
+        },
+        {
+          id: 6,
+          link: '/',
+          name: 'Tour Header Type',
+          items: [
+            {
+              id: 1,
+              name: 'Featured Image',
+              link: '/'
+            },
+            {
+              id: 2,
+              name: 'Video',
+              link: '/'
+            },
+          ]
+        },
+        {
+          id: 7,
+          link: '/',
+          name: 'Tour Categories',
+          items: [
+            {
+              id: 1,
+              name: 'Rural',
+              link: '/'
+            },
+            {
+              id: 2,
+              name: 'Snow & Ice',
+              link: '/'
+            },
+          ]
         },
       ]
     },
