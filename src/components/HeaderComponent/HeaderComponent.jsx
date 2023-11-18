968198112977ca012ec8ef0a517e10732ab2bd48
Nig36 <106310322+pdat2000@users.noreply.github.com>
2023-11-19 01:44:29 +0700

    scroll header

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -7,62 +7,62 @@ import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
 import {menus} from '../../utils'
 import './style.scss'
 
-
-const HeaderComponent = () => {
+const HeaderComponent = ({show}) => {
   return (
-    <Box sx={{ flexGrow: 1 }} className='header'>
-      <Grid container spacing={2} style={{ padding: '26px 0' }}>
-        <Grid item xs={6} md={4}>
-          <Link href="/" className='header-logo'>
-          <img src={Logo1} alt="gtour" />
-          </Link>
-        </Grid>
-        <Grid item xs={6} md={8} style={{ display:'flex', justifyContent:'space-between', alignItems:'flex-start' }}>
-          <div className='header-menu'>
-            {menus.map((menu)=>{
-              return ( <div className='header-item' key={menu.id}>
-              <div className='header-list'>
-                 <Link href={menu.link}>{menu.name}</Link>
-                 <KeyboardArrowDownOutlinedIcon fontSize='16px' color='#fff'/>
-              </div>
-              <p className='header-line'></p>
-              <ul className='header-menu2'>
-                  {menu.items.map((item2)=>{
-                    return (<li className='header-item2' key={item2.id}>
-                      <div className='header-item2-list'>
-                        <a href={item2.link}>{item2.name}</a>
-                        {Array.isArray(item2.items) && item2.items.length > 0 &&
-                        <ChevronRightOutlinedIcon fontSize='16px'/>}
-                      </div>
-                      {Array.isArray(item2.items) && item2.items.length > 0 &&
-                        <ul className='header-menu3'>
-                          {item2.items.map((item3)=>{
-                            return (
-                              <li className='header-item3' key={item3.id}>
-                                <a href={item3.link}>{item3.name}</a>
-                              </li>
-                            )
-                          })}
-                      </ul>}
-                  </li>)
-                  })}
-               </ul>
-             </div>)
-            })}
-          </div>
-          <div className='header-menu-right'>
-            <Link href="/">
-              <MenuIcon style={{ color: '#fff', fontSize: '17px' }}/>
+    <div>
+      {show && <Box sx={{ flexGrow: 1 }} className='header'>
+        <Grid container spacing={2} style={{ padding: '18px 0 26px' }}>
+          <Grid item xs={6} md={4}>
+            <Link href="/" className='header-logo'>
+            <img src={Logo1} alt="gtour" />
             </Link>
-              <Badge badgeContent={1} color="error">
-                <Link href="/">
-                    <ShoppingCartOutlinedIcon style={{ color: '#fff', fontSize: '17px' }} />
-                </Link>
-              </Badge>
-          </div>
+          </Grid>
+          <Grid item xs={6} md={8} style={{ display:'flex', justifyContent:'space-between', alignItems:'flex-start' }}>
+            <div className='header-menu'>
+              {menus.map((menu)=>{
+                return ( <div className='header-item' key={menu.id}>
+                <div className='header-list'>
+                   <Link href={menu.link}>{menu.name}</Link>
+                   <KeyboardArrowDownOutlinedIcon fontSize='16px' color='#fff'/>
+                </div>
+                <p className='header-line'></p>
+                <ul className='header-menu2'>
+                    {menu.items.map((item2)=>{
+                      return (<li className='header-item2' key={item2.id}>
+                        <div className='header-item2-list'>
+                          <a href={item2.link}>{item2.name}</a>
+                          {Array.isArray(item2.items) && item2.items.length > 0 &&
+                          <ChevronRightOutlinedIcon fontSize='16px'/>}
+                        </div>
+                        {Array.isArray(item2.items) && item2.items.length > 0 &&
+                          <ul className='header-menu3'>
+                            {item2.items.map((item3)=>{
+                              return (
+                                <li className='header-item3' key={item3.id}>
+                                  <a href={item3.link}>{item3.name}</a>
+                                </li>
+                              )})}
+                        </ul>}
+                    </li>)
+                    })}
+                 </ul>
+               </div>)
+              })}
+            </div>
+            <div className='header-menu-right'>
+              <Link href="/">
+                <MenuIcon style={{ color: '#fff', fontSize: '17px' }}/>
+              </Link>
+                <Badge badgeContent={1} color="error">
+                  <Link href="/">
+                      <ShoppingCartOutlinedIcon style={{ color: '#fff', fontSize: '17px' }} />
+                  </Link>
+                </Badge>
+            </div>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>}
+    </div>
   );
 };
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from 'react';
 import {Container} from '@mui/material';
 import HeaderComponent from '../../components/HeaderComponent/HeaderComponent';
 import './style.scss'
 
 const HomePage = () => {
+  const[show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const controlNavbar = () => {
+    console.log('scroll', window.scrollY);
+    if (window.scrollY > lastScrollY) { 
+      setShow(false); 
+    } else { 
+      setShow(true);  
+    }
+    setLastScrollY(window.scrollY); 
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar);
+    return () => {
+       window.removeEventListener('scroll', controlNavbar);
+    };
+  }, [lastScrollY]);
   return (
-    <Container maxWidth="1425">
-      <div className='container'>
-        <HeaderComponent/>
-      </div>
-    </Container>
+    <>
+      <HeaderComponent show={show}/>
+      <Container maxWidth="1425">
+        <div className='container'>
+          <div style={{ color:'red' }}>
+            âbc
+          </div>
+        </div>
+      </Container>
+    </>
   );
 };
 
