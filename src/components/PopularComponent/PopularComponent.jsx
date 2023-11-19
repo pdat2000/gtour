f6f71243b886946e70d6e930d2315fbf8784a047
Nig36 <106310322+pdat2000@users.noreply.github.com>
2023-11-19 21:41:28 +0700

    reponsive 3

--- src/components/PopularComponent/PopularComponent.jsx
+++ src/components/PopularComponent/PopularComponent.jsx
@@ -4,9 +4,9 @@ import './style.scss'
 
 const PopularComponent = ({populars}) => {
   return (
-    <Grid container spacing={1} style={{ marginTop: '35px' }}>
+    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} style={{ marginTop: '35px' }}>
       {populars.map((popular)=>{
-        return ( <Grid item xs={3} key={popular.id}>
+        return ( <Grid item sm={3} sx={1} key={popular.id}>
           <Link href='/'>
             <div className="popular-card">
               <img className='popular-image' src={popular.image} alt={popular.title} />
