555a9fd79131833ab3daf8ca6bd38b568e3803a9
Nig36 <106310322+pdat2000@users.noreply.github.com>
2023-11-19 21:29:48 +0700

    reponsive 2

--- src/components/CopyRightComponent/CopyRightComponent.jsx
+++ src/components/CopyRightComponent/CopyRightComponent.jsx
@@ -2,13 +2,13 @@ import './style.scss'
 const CopyRightComponent = () => {
   return (
     <div className="footer1">
-      <div className="footer1-copyright">© Copyright Grand Tour Theme Demo - Theme by ThemeGoods</div>
       <div className="footer1-list">
         <div className="footer1-item">Home</div>
         <div className="footer1-item">Tour</div>
         <div className="footer1-item">Blog</div>
         <div className="footer1-item">Purchase Theme</div>
       </div>
+      <div className="footer1-copyright">© Copyright Grand Tour Theme Demo - Theme by ThemeGoods</div>
     </div>
   );
 };
