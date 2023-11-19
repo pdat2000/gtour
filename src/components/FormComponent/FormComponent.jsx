bd016f2dcb0ae90b6a7f0471982e71bb7f44a445
Nig36 <106310322+pdat2000@users.noreply.github.com>
2023-11-19 18:37:28 +0700

    input component

--- src/components/FormComponent/FormComponent.jsx
+++ src/components/FormComponent/FormComponent.jsx
@@ -1,15 +1,21 @@
 import {Grid} from '@mui/material';
 import ButtonComponent from '../ButtonComponent/ButtonComponent';
 import InputComponent from '../InputComponent/InputComponent';
+import SearchIcon from '@mui/icons-material/Search';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 
 const FormComponent = () => {
   return (
     <Grid container spacing={1}  style={{ padding: '20px 20px 0' }}>
       <Grid item xs={3}>
+        <InputComponent placeholder='Destination, city' icon={<SearchIcon/>} />
+      </Grid>
+      <Grid item xs={3}>
         <InputComponent />
       </Grid>
-      <Grid item xs={3}></Grid>
-      <Grid item xs={3}></Grid>
+      <Grid item xs={3}>
+        <InputComponent />
+      </Grid>
       <Grid item xs={3}>
         <ButtonComponent
            variant='contained' 
@@ -25,6 +31,15 @@ const FormComponent = () => {
         >
           Search
         </ButtonComponent>
+      </Grid>
+      <Grid item xs={3}>
+        <InputComponent placeholder='Destination, city' icon={<SearchIcon/>} />
+      </Grid>
+      <Grid item xs={3}>
+        <InputComponent placeholder='Destination, city' icon={<SearchIcon/>} />
+      </Grid>
+      <Grid item xs={3}>
+        <InputComponent placeholder='Max budget ex. 500' icon={<AttachMoneyIcon/>} />
       </Grid>
     </Grid>
   );
--- src/components/InputComponent/InputComponent.jsx
+++ src/components/InputComponent/InputComponent.jsx
@@ -1,9 +1,16 @@
-
-const InputComponent = () => {
+import {OutlinedInput, InputAdornment} from '@mui/material'
+const InputComponent = ({placeholder, icon}) => {
   return (
-    <div>
-      
-    </div>
+    <OutlinedInput
+      placeholder={placeholder}
+      fullWidth
+      endAdornment={
+        <InputAdornment position="end">
+          {icon}
+        </InputAdornment>
+          }
+          sx={{ backgroundColor:'#fff', height:'37px', width: '283px' }}
+        />
   );
 };
 
